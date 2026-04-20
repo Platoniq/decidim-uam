8efd16f71a4157183fbc8ff48012873de3834604
Fran Bolívar <francisco.bolivar@nazaries.com>
2026-04-20 15:22:14 +0200

    Remove provider name decorator

--- config/initializers/decidim_overrides.rb
+++ config/initializers/decidim_overrides.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 Rails.application.config.to_prepare do
-  Decidim::OmniauthHelper.prepend(Decidim::OmniauthHelperDecorator)
   Decidim::Devise::OmniauthRegistrationsController.include(Decidim::Devise::OmniauthRegistrationsControllerOverride)
 end
